3b969ae76b41c9047ae59ff17fa08b2e3a5cf4a2
Jeff Nadolna <jnadolna@csumb.edu>
2019-03-11 20:41:55 +0000

    Practice 6 Commit

---
+++ Practice/p6/api/getRandomProduct.php
@@ -0,0 +1,43 @@
+<?php
+  session_start();
+  $httpMethod = strtoupper($_SERVER['REQUEST_METHOD']);
+  switch($httpMethod) {
+    case "OPTIONS":
+      // Allows anyone to hit your API, not just this c9 domain
+      header("Access-Control-Allow-Headers: X-ACCESS_TOKEN, Access-Control-Allow-Origin, Authorization, Origin, X-Requested-With, Content-Type, Content-Range, Content-Disposition, Content-Description");
+      header("Access-Control-Allow-Methods: POST, GET");
+      header("Access-Control-Max-Age: 3600");
+      exit();
+    case "GET":
+      http_response_code(401);
+      break;
+    case 'POST':
+      // Get the body json that was sent
+      $rawJsonString = file_get_contents("php://input");
+      //var_dump($rawJsonString);
+      // Make it a associative array (true, second param)
+      $jsonData = json_decode($rawJsonString, true);
+      // Allow any client to access
+      header("Access-Control-Allow-Origin: *");
+      // Let the client know the format of the data being returned
+      header("Content-Type: application/json");
+      $products = array(
+            array("Microfiber Beach Towel", 40, 2),
+            array("Flip-flop Sandals", 30, 5),
+            array("Sunscreen 80SPF", 25, 3),
+            array("Plastic Flying Disc", 15, 4),
+            array("Beach Umbrella", 75, 1)
+          );
+      echo json_encode($products[rand(0, 4)]);
+      break;
+    case 'PUT':
+      // TODO: Access-Control-Allow-Origin
+      http_response_code(401);
+      echo "Not Supported";
+      break;
+    case 'DELETE':
+      // TODO: Access-Control-Allow-Origin
+      http_response_code(401);
+      break;
+  }
+?>

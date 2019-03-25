3123114ebab436e4029ba23c02776d09aca8800e
Jeff Nadolna <jnadolna@csumb.edu>
2019-03-25 06:04:09 +0000

    Lab 6 push

---
+++ Labs/Lab_6/dbConnection.php
@@ -0,0 +1,24 @@
+<?php
+
+function getDatabaseConnection($dbName = 'ottermart') {
+    $host = 'localhost';
+    $username = 'root';
+    $password = '';
+    
+    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $dbname = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+    } 
+
+    
+    $dbConn = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
+    
+    $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    return $dbConn;
+}
+
+?>
